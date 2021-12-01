fde3550b2529b254e1b27459bfbf5909b6198b59
Maximillian Vincent <maximillianvincent@maximilliansmbp.myfiosgateway.com>
2021-12-01 14:15:36 -0500

    Added query for current employees ready for retirement.

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
-CREATE TABLE dept_employee (
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
