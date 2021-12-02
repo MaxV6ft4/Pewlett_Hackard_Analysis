72f3b19a824e6ff28425b6ff251e0ebb95496b53
Maximillian Vincent <maximillianvincent@maximilliansmbp.myfiosgateway.com>
2021-12-02 15:19:19 -0500

    Added seven CSV files and updated queries.

--- queries.sql
+++ queries.sql
@@ -63,3 +63,100 @@ FROM retirement_info as ri
 LEFT JOIN dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
+-- Check the table
+SELECT * FROM current_emp;
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO current_emp_count
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+-- Check the table
+SELECT * FROM current_emp_count;
+
+-- Order the salaries to_date column in descending fashion
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Salaries of those both currently employed and eligible for retirment
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+-- Check the table
+SELECT * FROM emp_info;
+
+-- List of managers per department
+SELECT dm.dept_no,
+	   d.dept_name,
+	   dm.emp_no,
+	   ce.last_name,
+	   ce.first_name,
+	   dm.from_date,
+	   dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+	JOIN departments as d
+		ON (dm.dept_no = d.dept_no)
+	JOIN current_emp as ce
+		ON (dm.emp_no = ce.emp_no);
+-- Check the table
+SELECT * FROM manager_info;
+
+-- List of retirees per department
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+JOIN departments as d
+ON (de.dept_no = d.dept_no);
+-- Check the table
+SELECT * FROM dept_info;
+
+-- List of retirees in sales department
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_info
+FROM current_emp as ce
+JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+-- Check the table
+SELECT * FROM sales_info;
+
+-- List of retirees in both sales and development depts
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO sales_dev_info
+FROM current_emp as ce
+JOIN dept_emp as de
+ON (ce.emp_no = de.emp_no)
+JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
+-- Check the table
+SELECT * FROM sales_dev_info;
